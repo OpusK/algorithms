6f5e549e98dba56fea19507f769bd45361d3647f
Kei <opusk44@gmail.com>
2022-04-24 00:41:22 +0900

    add day 02 to index.ts

--- quiz/adventofcode/typescript/src/index.ts
+++ quiz/adventofcode/typescript/src/index.ts
@@ -1,5 +1,9 @@
 import { AoC2021Day01 } from "./2021_day01";
+import { AoC2021Day02 } from "./2021_day02";
 
 console.log("======= 2021 =======");
 console.log("==== Day01 ====");
 AoC2021Day01();
+console.log("==== Day02 ====");
+AoC2021Day02();
+console.log("======= ========");
